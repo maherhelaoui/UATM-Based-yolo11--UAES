76922d522476391c7853c26d2a721c0777a67a99
maherhelaoui <maher.helaoui@gmail.com>
2025-03-20 15:16:05 +0100

    Update TrainYOLO11G1.py

--- TrainYOLO11G1.py
+++ TrainYOLO11G1.py
@@ -1,10 +1,10 @@
-#If you use YOLO11++ model from this repository in your work, please cite it using the following format:
+#If you use YOLO11Gi model from this repository in your work, please cite it using the following format:
 
 #BibTeX
-#@article{hel25YOLO11++UAES,
+#@article{hel25YOLO11G1ANES,
 #  title={Universal Autonomous Driving System: Innovation with YOLO11++-based Universal Autonomous Expert System},
 #  author={Maher, Helaoui and Sahbi, Bahroun and Ezzeddine, Zagrouba},
-#  journal={Machine Vision and Applications (Submited)},
+#  journal={Neural Computing and Applications (Submited)},
 #  url = {https://github.com/maherhelaoui/UATM-Based-yolo11--UAES/},
 #  year={2025},
 #  publisher={Springer}
@@ -25,26 +25,26 @@ from collections import deque
 
 
 # YOLO11 is trained on coco dataset.
-#Train YOLO11++ on different Datasets 
+#Train YOLO11G1 on different Datasets 
 
 model = YOLO("yolo11n.yaml")
 # Charger les modèles YOLO
-#model3 = YOLO("yolo11++MP.pt")  # if model.train(data="medical-pills.yaml", epochs=100)
-#model3 = YOLO("yolo11++GW.pt")  # if model.train(data="GlobalWheat2020.yaml", epochs=100)
-#model3 = YOLO("yolo11++S.pt")  # if model.train(data="signature.yaml", epochs=100)
-#model3 = YOLO("yolo11++AW.pt")  # if model.train(data="african-wildlife.yaml", epochs=100)
-#model3 = YOLO("yolo11++BT.pt")  # if model.train(data="brain-tumor.yaml", epochs=100)
-model3 = YOLO("yolo11++CC128.pt")  # if model.train(data="coco128.yaml", epochs=100)
+#model3 = YOLO("yolo11G1MP.pt")  # if model.train(data="medical-pills.yaml", epochs=100)
+#model3 = YOLO("yolo11G1GW.pt")  # if model.train(data="GlobalWheat2020.yaml", epochs=100)
+#model3 = YOLO("yolo11G1S.pt")  # if model.train(data="signature.yaml", epochs=100)
+#model3 = YOLO("yolo11G1AW.pt")  # if model.train(data="african-wildlife.yaml", epochs=100)
+#model3 = YOLO("yolo11G1BT.pt")  # if model.train(data="brain-tumor.yaml", epochs=100)
+model3 = YOLO("yolo11G1CC128.pt")  # if model.train(data="coco128.yaml", epochs=100)
 model = model3
 
 
 
-#results = model.train(data="medical-pills.yaml", epochs=100, imgsz=640) # Train MP Dataset use model yolo11++MP.pt
-#results = model.train(data="GlobalWheat2020.yaml", epochs=100, imgsz=640) # Train GW Dataset use model yolo11++GW.pt
-#results = model.train(data="signature.yaml", epochs=100, imgsz=640) # Train SD Dataset use model yolo11++S.pt
-#results = model.train(data="african-wildlife.yaml", epochs=100, imgsz=640) # Train AW Dataset use model yolo11++AW.pt
-#results = model.train(data="brain-tumor.yaml", epochs=100, imgsz=640) # Train BT Dataset use model yolo11++BT.pt
-results = model.train(data="coco128.yaml", epochs=100) # Train CC128 Dataset use model yolo11++CC128.pt 
+#results = model.train(data="medical-pills.yaml", epochs=30, imgsz=640) # Train MP Dataset use model yolo11++MP.pt
+#results = model.train(data="GlobalWheat2020.yaml", epochs=30, imgsz=640) # Train GW Dataset use model yolo11++GW.pt
+#results = model.train(data="signature.yaml", epochs=30, imgsz=640) # Train SD Dataset use model yolo11++S.pt
+#results = model.train(data="african-wildlife.yaml", epochs=30, imgsz=640) # Train AW Dataset use model yolo11++AW.pt
+#results = model.train(data="brain-tumor.yaml", epochs=30, imgsz=640) # Train BT Dataset use model yolo11++BT.pt
+results = model.train(data="coco128.yaml", epochs=30) # Train CC128 Dataset use model yolo11++CC128.pt 
 
 
 
